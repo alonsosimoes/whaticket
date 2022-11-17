23ee44ae61e06a8d50039d74377574a9e07c37f1
alonsosimoes <45598711+alonsosimoes@users.noreply.github.com>
2022-11-17 11:05:56 -0400

    Update admin pass

--- backend/src/database/seeds/20200904070004-create-default-users.ts
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -8,7 +8,7 @@ module.exports = {
         {
           name: "Administrador",
           email: "admin@admin.com.br",
-          passwordHash: "$2a$08$WaEmpmFDD/XkDqorkpQ42eUZozOqRCPkPcTkmHHMyuTGUOkI8dHsq",
+          passwordHash: "$2a$08$B0R3oeWn71jQXiHkC5.Pvu.dKWzkwcnPmf3qgK/rQzv4VvtTg3pbS",
           profile: "admin",
           tokenVersion: 0,
           createdAt: new Date(),
